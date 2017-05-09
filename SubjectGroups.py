075d3d05c5ca67527668b545c9a65002537282ff
zaqs114 <zaqs@10g.pl>
2017-05-09 20:45:30 +0200

    another core functionality

--- SubjectGroups.py
+++ SubjectGroups.py
@@ -9,7 +9,6 @@ def subject_groups_menu():
                 Wybierz 2 aby usunąć grupę przedmiotową.
                 """)
     if choice == "0":
-        subject_group_write_to_file()
         os.system('cls')
         import Main
         Main.edit_information_menu()
@@ -44,138 +43,133 @@ def add_subject_group():
     choice = int(input("""Wybierz przedmiot dla którego chcesz utworzyć nową grupę..
                 """))
     choice -= 1
-    subjectGroupName = Classes.Subjects.subjectList[choice].Classes.Subjects.return_subject_name()
-
+    subjectGroupName = Classes.Subjects.subjectList[choice].subjectName
+    print(subjectGroupName)
     os.system('cls')
     choice=int(input("""Dodawanie grupy przedmiotowej.
     Wybierz 1 aby dodać informatyka do grupy.
     Wybierz 2 aby dodać matematyka do grupy
     """))
-    if choice == "1":
-        if not Classes.ITEmployee.itEmployeeList:
-            os.system('cls')
-            print("W bazie nie ma żadnych pracowników. Dodaj je, i spróbuj ponownie.")
-            subject_groups_menu()
-        j = 1
-        for i in Classes.ITEmployee.itEmployeeList:
-            print(j, '. ', i)
-            j += 1
-        choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
-                    """))
-        choice -= 1
-        teacherName = Classes.ITEmployee.itEmployeeList[choice].Classes.ITEmployee.return_employee()
-
+    if choice==1:
         os.system('cls')
+        teacherName=add_itemployee_to_group()
         choice = int(input("""Dodawanie grupy przedmiotowej.
-                    Wybierz 1 aby studentów dziennych do grupy.
-                    Wybierz 2 aby studentów zaocznych do grupy
-                    """))
-        if choice == "1":
-            os.system('cls')
+                            Wybierz 1 aby studentów dziennych do grupy.
+                            Wybierz 2 aby studentów zaocznych do grupy
+                            """))
+        if choice == 1:
             studentNames = []
             studentNames = add_daily_student_to_group()
-            Classes.SubjectGroups.subjectGroupsList.append(Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Dzienne", teacherName, studentNames))
+            Classes.SubjectGroups.subjectGroupsList.append(
+                Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Dzienne", teacherName, studentNames))
 
-        if choice == "2":
-            os.system('cls')
+        if choice==2:
             studentNames = []
             studentNames = add_weekend_student_to_group()
-            Classes.SubjectGroups.subjectGroupsList.append( Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Zaoczne", teacherName, studentNames))
-    else:
+            Classes.SubjectGroups.subjectGroupsList.append(Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Zaoczne", teacherName, studentNames))
+    if choice == 2:
         os.system('cls')
-        print("W bazie nie ma żadnych informatyków. Dodaj ich, i spróbuj ponownie.")
-        subject_groups_menu()
-    if choice == "2":
-        if not Classes.MathEmployee.mathEmployeeList:
-            j = 1
-            for i in Classes.MathEmployee.mathEmployeeList:
-                print(j, '. ', i)
-                j += 1
-            choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
-                        """))
-            choice -= 1
-            teacherName = Classes.MathEmployee.mathEmployeeList[choice].Classes.MathEmployee.return_employee()
-
-            os.system('cls')
-            choice = int(input("""Dodawanie grupy przedmiotowej.
-                        Wybierz 1 aby studentów dziennych do grupy.
-                        Wybierz 2 aby studentów zaocznych do grupy
-                        """))
-            if choice == "1":
-                os.system('cls')
-                studentNames = []
-                studentNames = add_daily_student_to_group()
-                Classes.SubjectGroups.subjectGroupsList.append(
-                    Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Dzienne", teacherName,
-                                          studentNames))
-
-            if choice == "2":
-                os.system('cls')
-                studentNames = []
-                studentNames = add_weekend_student_to_group()
-                Classes.SubjectGroups.subjectGroupsList.append(
-                    Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Zaoczne", teacherName,
-                                          studentNames))
-        else:
-            os.system('cls')
-            print("W bazie nie ma żadnych informatyków. Dodaj ich, i spróbuj ponownie.")
-            subject_groups_menu()
+        teacherName = add_mathemployee_to_group()
+        choice = int(input("""Dodawanie grupy przedmiotowej.
+                            Wybierz 1 aby dodać studentów dziennych do grupy.
+                            Wybierz 2 aby dodać studentów zaocznych do grupy
+                            """))
+        if choice == 1:
+            studentNames = []
+            studentNames = add_daily_student_to_group()
+            Classes.SubjectGroups.subjectGroupsList.append(
+                Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Dzienne", teacherName, studentNames))
 
 
+        if choice == 2:
+            studentNames = []
+            studentNames = add_weekend_student_to_group()
+            Classes.SubjectGroups.subjectGroupsList.append(
+                Classes.SubjectGroups(subjectGroupName, subjectGroupID, "Zaoczne", teacherName, studentNames))
     os.system('cls')
-    print("Pomyślnie dodano studenta")
+    print("Pomyślnie dodano grupe przedmiotowa")
     print()
     add_subject_group()
 
 
+def add_itemployee_to_group():
+    if not Classes.ITEmployee.itEmployeeList:
+        os.system('cls')
+        print("W bazie nie ma żadnych pracowników. Dodaj ich, i spróbuj ponownie.")
+        subject_groups_menu()
+    j = 1
+    for i in Classes.ITEmployee.itEmployeeList:
+        print(j, '. ', i)
+        j += 1
+    choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
+                        """))
+    choice -= 1
+    teacherName = Classes.ITEmployee.itEmployeeList[choice].firstname + " " + Classes.ITEmployee.itEmployeeList[choice].lastname
+    print(teacherName)
+    return teacherName
+
+
+def add_mathemployee_to_group():
+    if not Classes.MathEmployee.mathEmployeeList:
+        os.system('cls')
+        print("W bazie nie ma żadnych pracowników. Dodaj ich, i spróbuj ponownie.")
+        subject_groups_menu()
+    j = 1
+    for i in Classes.MathEmployee.mathEmployeeListt:
+        print(j, '. ', i)
+        j += 1
+    choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
+                        """))
+    choice -= 1
+    teacherName = Classes.MathEmployee.mathEmployeeList[choice].firstname + " " + Classes.MathEmployee.mathEmployeeList[choice].lastname
+    print(teacherName)
+    return teacherName
+
+
 def add_daily_student_to_group():
     if not Classes.DailyStudent.dailyStudentList:
-        j = 1
-        for i in Classes.DailyStudent.dailyStudentList:
-            print(j, '. ', i)
-            j += 1
-        choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
-                    """))
-        choice -= 1
-        studentNames = []
-        studentNames.append(Classes.DailyStudent.dailyStudentList[choice].Classes.DailyStudent.return_student())
-        choice=int(input("""
-        Wybierz 1 aby dodać kolejnego studenta.
-        Wybierz 0 aby powrócić"""))
-        if choice == "1":
-            add_daily_student_to_group()
-        else:
-            return studentNames
-
-    else:
         os.system('cls')
         print("W bazie nie ma żadnych studentów dziennych. Dodaj ich, i spróbuj ponownie.")
         subject_groups_menu()
+    j = 1
+    for i in Classes.DailyStudent.dailyStudentList:
+        print(j, '. ', i)
+        j += 1
+    choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
+                """))
+    choice -= 1
+    studentNames = []
+    studentNames.append(Classes.DailyStudent.dailyStudentList[choice].name + Classes.DailyStudent.dailyStudentList[choice].surname )
+    choice=int(input("""
+    Wybierz 1 aby dodać kolejnego studenta.
+    Wybierz cokolwiek aby powrócić"""))
+    if choice == 1:
+        add_daily_student_to_group()
+    else:
+        return studentNames
 
 
 def add_weekend_student_to_group():
     if not Classes.WeekendStudent.weekendStudentList:
-        j = 1
-        for i in Classes.WeekendStudent.weekendStudentList:
-            print(j, '. ', i)
-            j += 1
-        choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
-                       """))
-        choice -= 1
-        studentNames = []
-        studentNames.append(Classes.WeekendStudent.weekendStudentList[choice].Classes.WeekendStuden.return_student())
-        choice = int(input("""
-           Wybierz 1 aby dodać kolejnego studenta.
-           Wybierz 0 aby powrócić"""))
-        if choice == "1":
-            add_daily_student_to_group()
-        else:
-            return studentNames
-
-    else:
         os.system('cls')
         print("W bazie nie ma żadnych studentów dziennych. Dodaj ich, i spróbuj ponownie.")
         subject_groups_menu()
+    j = 1
+    for i in Classes.WeekendStudent.weekendStudentList:
+        print(j, '. ', i)
+        j += 1
+    choice = int(input("""Wybierz pracownika którego chcesz dodać do grupy
+                   """))
+    choice -= 1
+    studentNames = []
+    studentNames.append(Classes.WeekendStudent.weekendStudentList[choice].name +Classes.WeekendStudent.weekendStudentList[choice].surname)
+    choice = int(input("""
+       Wybierz 1 aby dodać kolejnego studenta.
+       Wybierz 0 aby powrócić"""))
+    if choice == 1:
+        add_daily_student_to_group()
+    else:
+        return studentNames
 
 
 def delete_subject_group():
